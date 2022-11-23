e2895f84e2e8c8158ffafb368f1753261612e18d
Akash Pandey <75413146+1337-L3V1ATH0N@users.noreply.github.com>
2022-11-23 14:16:55 +0530

    Update Endian Byte Flipper.py

--- BufferOverflow/Endian Byte Flipper.py
+++ BufferOverflow/Endian Byte Flipper.py
@@ -1,13 +1,15 @@
-print("== Endian Byte Flipper ==\nDeveloped with ❤ by Akash Pandey")
-endian=input("\n[+] Enter your jump ESP address: ")
-increment=0
-if len(endian)==8:
-    end=endian[::-2]
-    ian=endian[-2::-2]
-    while increment != 4:
-        print("\\x"+ian[increment]+end[increment],end="")
-        increment+=1
-            
-               
-else:
-    print("Check Address")
+try:
+    print("== Endian Byte Flipper ==\nDeveloped with ❤ by Akash Pandey")
+    endian=input("\n[+] Enter your jump ESP address: ")
+    increment=0
+    if len(endian)==8:
+        end=endian[::-2]
+        ian=endian[-2::-2]
+        print("[+] Flipped: ",end="")
+        while increment != 4:
+            print("\\x"+ian[increment]+end[increment],end="")
+            increment+=1        
+    else:
+        print("[!] Check Address")
+except:
+    print("[!] Exiting!")
